881965e1f62e1e81cda372a4399629d9f922b264
Damian Lis <foxprogramming93@gmail.com>
2021-07-30 10:06:02 +0200

    Added new methods to the linked list

--- linkedList.js
+++ linkedList.js
@@ -47,4 +47,108 @@ function LinkedList() {
 
     length++;
   };
+
+  this.remove = function (element) {
+    let currentNode = head;
+    let previousNode;
+
+    if (currentNode.element === element) {
+      head = currentNode.next;
+    } else {
+      while (currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      previousNode.next = currentNode.next;
+    }
+
+    length--;
+  };
+
+  this.isEmpty = function () {
+    return length === 0;
+  };
+
+  this.indexOf = function (element) {
+    let currentNode = head;
+    let index = -1;
+
+    while (currentNode) {
+      index++;
+      if (currentNode.element === element) {
+        return index;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return -1;
+  };
+
+  this.elementAt = function (index) {
+    let currentNode = head;
+    let count = 0;
+
+    while (count < index) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    return currentNode.element;
+  };
+
+  this.addAt = function (index, element) {
+    const node = new Node(element);
+
+    let previousNode;
+    let currentNode = head;
+    let currentIndex = 0;
+
+    if (index > length) return false;
+
+    if (index === 0) {
+      node.next = currentNode;
+      head = node;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      node.next = currentNode;
+      previousNode.next = node;
+    }
+    length++;
+  };
+
+  this.removeAt = function (index) {
+    let previousNode;
+    let currentNode = head;
+    let currentIndex = 0;
+
+    if (index < 0 || index >= length) return null;
+
+    if (index === 0) {
+      head = currentNode.next;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      previousNode.next = currentNode.next;
+    }
+    length--;
+    return currentNode.element;
+  };
 }
+
+const linkedList = new LinkedList();
+
+linkedList.add("Damian");
+linkedList.add("Kasia");
+linkedList.add("Mateusz");
+
