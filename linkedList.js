d9827faed161c403e075ad77c5219f6ec40980aa
Damian Lis <foxprogramming93@gmail.com>
2021-08-08 21:11:16 +0200

    Added new implementation of the linked list data structure

--- linkedList.js
+++ linkedList.js
@@ -152,3 +152,122 @@ linkedList.add("Damian");
 linkedList.add("Kasia");
 linkedList.add("Mateusz");
 
+// --------------------------------------------------------------------------------
+
+class LinkedList2 {
+  constructor() {
+    this.head = null; // First element of the list
+    this.tail = null; // Last element of the list
+    // Its a extra convinient to store the tails becouse we can have quick access to the last element
+  }
+
+  append(value){
+    const newNode = {value: value, next:null}
+    if(this.tail){
+      this.tail.next = newNode
+      // Here we update the next node of the last element of the list
+    }
+    this.tail = newNode
+    // Here we update the last element of the list
+    if(!this.head){
+      this.head = newNode
+    }
+  }
+
+  prepend(value){
+    const newNode = {value: value, next:this.head}
+    this.head = newNode
+
+    if(!this.tail){
+      this.tail = newNode
+    }
+  }
+
+  insertAfter(value, afterValue){
+    const existingNode = this.find(afterValue)
+
+    if(existingNode){
+      const newNode = {value: value, next:existingNode.next}
+      existingNode.next = newNode
+    }
+  }
+
+  find(){
+    if(!this.head){
+      return null
+    }
+
+    const newNode = {value: value, next:null}
+
+    let currNode = this.head
+    while(currNode){
+     if(currNode.value === value){
+      return currNode
+     }
+     currNode = currNode.next
+    }
+
+    return null
+  }
+
+  delete(value){
+    if(!this.head){
+      return null
+    }
+
+    while(this.head && this.head.value === value){
+      // We introduced here while loop becouse after deleting head node the next node would have the same value as previons node
+      this.head = this.head.next
+    }
+
+    let currNode = this.head
+    while(currNode.next){
+      if(currNode.next.value === value){
+        currNode.next = currNode.next.next
+      }else{
+        currNode = currNode.next
+      }
+    }
+
+    if(this.tail.value === value){
+      this.tail = currentNode
+    }
+
+  }
+
+  toArray(){
+    const elements = []
+
+    let currNode = this.head
+    while(currNode){
+      elements.push(currNode)
+      currNode = currNode.next
+    }
+
+    return elements
+  }
+}
+
+const linkedList2 = new LinkedList2()
+
+linkedList2.append(1)
+linkedList2.append(2)
+linkedList2.append(3)
+linkedList2.prepend(5)
+
+console.log(linkedList2.toArray())
+
+// Few comments:
+// - the main reason for using this kind of data structure is memory managment. You didn't have to specify (occupy) the size in advance,
+// - nowadays, JS has dynamic arrays (dynamic resizing built in) and memory isn't really the primary issue in js apps,
+// - linked list can be useful if you do a lot of inserions at the begining of lists - linked lists are faster than arrays at this
+
+// Time complexity compare to arrays:
+//                           Linked list             Arrays
+// Element access:             O(n)                  O(1) <= advantage!!!
+// Inserion at end:            O(1)                  O(1)
+// Inserion at begining:       O(1) <= advantage!!!  O(n) <= becouse all index of the array must move 
+// Insetion in the middle:  Search time + O(1)       O(n)
+// Search elements:            O(n)                  O(n)
+
+
